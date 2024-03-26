8992de1f8844a22e1ac7c747f1b92417326ee9f5
Leandro Lima <lleandrorj@gmail.com>
2024-03-26 13:57:38 -0300

    feat: :sparkles: encapsulamento com C#

---
+++ Models/ContaCorrente.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace exemplo_poo.Models
+{
+    public class ContaCorrente
+    {
+        public ContaCorrente(int numeroConta, decimal saldoInicial)
+        {
+            NumeroConta = numeroConta;
+            saldo = saldoInicial;
+        }
+        public int NumeroConta { get; set; }
+
+        private decimal saldo;
+
+        public void Sacar(decimal valor)
+        {
+            if (saldo >= valor)
+            {
+                saldo -= valor;
+                Console.WriteLine($"Saque de R${valor.ToString("F2")} efetuado com sucesso");
+            }
+            else
+            {
+                Console.WriteLine($"Saldo insuficiente para o saque de R${valor.ToString("F2")}");
+            }
+        }
+        public void ExibirSaldo()
+        {
+            Console.WriteLine($"Seu saldo atual é de R${saldo.ToString("F2")}.");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,16 +3,16 @@ using exemplo_poo.Models;
 
 internal class Program
 {
-    private static void Main(string[] args)
-    {
-      // ContaCorrente conta = new(123, 1117);
-      // conta.ExibirSaldo();
-      // conta.Sacar(1170);
-      // conta.ExibirSaldo();
+  private static void Main(string[] args)
+  {
+    ContaCorrente conta = new(123, 1117);
+    conta.ExibirSaldo();
+    conta.Sacar(1170);
+    conta.ExibirSaldo();
 
-        Pessoa pessoa = new();
-        pessoa.Nome = "Leandro";
-        pessoa.Idade = 38;
-        pessoa.Apresentar();
-    }
+    // Pessoa pessoa = new();
+    // pessoa.Nome = "Leandro";
+    // pessoa.Idade = 38;
+    // pessoa.Apresentar();
+  }
 }

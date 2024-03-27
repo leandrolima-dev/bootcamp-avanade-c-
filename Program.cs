2f6ae6e7fdf415d212f3a358557428f857909a4a
Leandro Lima <lleandrorj@gmail.com>
2024-03-27 13:36:11 -0300

    feat: :sparkles: POO - classe selada (sealed) em C#""

--- Program.cs
+++ Program.cs
@@ -33,15 +33,24 @@ internal class Program
     // corrente.Creditar(2000);
     // corrente.ExibirSaldo();
 
-    //Instanciando um objeto de uma classe
-    Aluno aluno = new("")
-    {
-      Nome = "Leandro",
-      Idade = 38,
-      Nota = 10,
-      Email = "aluno@example.com"
-    };
-    aluno.Apresentar();
+    //Instanciando um objeto de uma classe, de forma direta 
+    // Aluno aluno = new("")
+    // {
+    //   Nome = "Leandro",
+    //   Idade = 38,
+    //   Nota = 10,
+    //   Email = "aluno@example.com"
+    // };
+    // aluno.Apresentar();
 
+    //Classe não pode ser instanciada, pois Diretor herda de Professor, mas não pode ser herdada devido a palavra reservada "sealed"
+    Diretor diretor = new Diretor
+    {
+        Idade = 38,
+        Nome = "Leandro",
+        Email = "leandro@example.com",
+        Salario = 2000
+    };
+    diretor.Apresentar();
   }
 }

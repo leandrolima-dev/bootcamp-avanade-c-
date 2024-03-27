c3ab9870a1c2d05bc86712771b487ddaa26ad805
Leandro Lima <lleandrorj@gmail.com>
2024-03-27 14:30:52 -0300

    feat: :sparkles: POO - interfaces em C#
    Classes Implementando Interfaces, como se fossem contratos a serem usados de modo obrigatorio

--- obj/Debug/net8.0/exemplo_poo.AssemblyInfo.cs
+++ obj/Debug/net8.0/exemplo_poo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("exemplo_poo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cdda08fb74a478d856b6e36e8d25ba04940767ae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57dba804e0858bf6bb97c718d48b0bd3c06df21a")]
 [assembly: System.Reflection.AssemblyProductAttribute("exemplo_poo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("exemplo_poo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
